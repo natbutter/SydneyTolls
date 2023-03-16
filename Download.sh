2d84f532c43e2fe3379e2c93c668947b45cec125
Nathaniel Butterworth <natbutter@gmail.com>
2023-03-17 08:06:53 +1100

    made download easier

---
+++ Download.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Downloads all the data from https://nswtollroaddata.com/data-download/
+# Set the current year and run
+cd data
+start_year=2009
+end_year=2023
+
+#CCT (2009Q2 - present)
+
+for TOLL in CCT M2 LCT ED M4 M7 NCX M5E M5; do
+  for i in $(seq $start_year $end_year); do
+    for j in {1..4}; do
+      if wget -q https://s3.ap-southeast-2.amazonaws.com/accc-assetdata-prod/data/accc/upload/asset/$TOLL/${TOLL}_traffic-data_FY${i}_Q${j}_csv.zip; then
+        echo "$TOLL $i Q$j"
+      else
+      echo "No data for $TOLL $i Q$j"
+      fi
+    done;
+  done;
+done
+
+#unzip it all
+unzip '*.zip'
+
+#Remove zips
+#rm *.zip
+#Remove M7 "trips" (keep "passages" as they are equivalent to other roads' trips.)
