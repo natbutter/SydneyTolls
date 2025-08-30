bfc6ec2ea9b721827858b088e7672efcba54f9fd
natbutter <natbutter@gmail.com>
2025-08-30 15:18:02 +1000

    Update Download.sh to 2025

--- Download.sh
+++ Download.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
-
-# Downloads all the data from https://nswtollroaddata.com/data-download/
-# Set the current year and run
 mkdir -p data
 cd data
 start_year=2009
-end_year=2024
+end_year=2025
 
 #All roads
-
-for TOLL in CCT M2 LCT ED M4 M7 NCX M5E M5; do
+for TOLL in CCT M2 LCT ED M4 M5E M5 NCX WCX M7; do
   for i in $(seq $start_year $end_year); do
     for j in {1..4}; do
       if wget -q https://s3.ap-southeast-2.amazonaws.com/accc-assetdata-prod/data/accc/upload/asset/$TOLL/${TOLL}_traffic-data_FY${i}_Q${j}_csv.zip; then
@@ -21,8 +16,11 @@ for TOLL in CCT M2 LCT ED M4 M7 NCX M5E M5; do
   done;
 done
 
-#unzip it all, note m5 trips will overwrite one another for early data.
+#unzip it all, m5 trips will overwrite one another for data prior to 1 Oct 2019.
 unzip '*.zip'
 
 #Remove zips
 rm *.zip 
+
+#Re-zip all files into one archive for upload
+zip tolls.zip *.csv
